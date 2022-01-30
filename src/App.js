612f440065672ea010dfa4103d873f975807e1c7
Aritra Banerjee <aritra.ban11@gmail.com>
2022-01-30 10:49:58 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -3,52 +3,31 @@ import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
 
-
 function App() {
   const [showAddTask, setshowAddTask] = useState(false)
-  const [tasks, setTasks] = useState(
-    [
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            date: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at School',
-            date: 'Feb 6th at 1:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            date: 'Feb 5th at 2:30pm',
-            reminder: false,
-        }
-    ])
+  const [tasks, setTasks] = useState([])
 
-// Delete Task
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task) => task.id !== id ))
-}
-
-// Toggle Reminder
-const toggleReminder = (id) => {
-  setTasks(
-    tasks.map(
-      (task) => task.id === id
-      ? {...task, reminder: !task.reminder} : task 
-    )
-  )
-}
-
-// Add Task
-const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task, }
-    setTasks([...tasks, newTask])
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id ))
   }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map(
+        (task) => task.id === id
+        ? {...task, reminder: !task.reminder} : task 
+      )
+    )
+  }
+
+  // Add Task
+  const addTask = (task) => {
+      const id = Math.floor(Math.random() * 10000) + 1
+      const newTask = { id, ...task, }
+      setTasks([...tasks, newTask])
+    }
 
   return (
     <div className="container">
